64b2e772c69d2eb8cafcc54135d3d4560abb817a
Marchete <marchena.carlos@gmail.com>
2017-07-17 09:59:54 -0500

    Adding solution to the exercise

--- projects/voronoi/tron/voronoi.js
+++ projects/voronoi/tron/voronoi.js
@@ -1,25 +1,22 @@
-// Color each point
+// Create the Voronoi areas
 
 function fillVoronoi (tiles, players) {
   /*
-    Modify the `site` property of *each* tile according to be a *reference* to one of the players
+    Modify the `site` property of *each* tile to be a *reference* to one of the players
     given as parameter. The tile must be in that player's Voronoi site.
     ex:
       tiles[0].site = players[0];
       tiles[1].site = players[1];
-    Then return the tiles array
   */
-  // TODO
   for(var t in tiles) {
     var tile = tiles[t];
-
     tile.site = null;
+	// TODO: Select the nearest player, assigning it to tile.site
   }
 }
 
 function distance(pointA, pointB) {
-  // TODO implement the manhattan distance function
+  // TODO: Implement the manhattan distance function
   // Both tiles and players have a `x` and a `y` property
-
   return 0;
 }
